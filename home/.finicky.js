3a4810fa0b293e68853e5c90d0eb77a9b4df74e8
TravisCarden <travis.carden@gmail.com>
2019-11-13 00:27:02 -0500

    Added Mac App Store handler to Finicky.

--- home/.finicky.js
+++ home/.finicky.js
@@ -10,7 +10,15 @@ module.exports = {
       })
     }
   ],
+  // Find bundle IDs with this command:
+  // $ osascript -e 'id of app "ReplaceWithAppName"'
+  // @see https://superuser.com/q/346369/1109136
   handlers: [
+    // Open Mac app links in App Store app.
+    {
+      match: /apps.apple.com\/([^\/]*\/)?app\//,
+      browser: "com.apple.AppStore"
+    },
     // Open Zoom meeting links in Zoom app.
     {
       match: /zoom.us\/j\//,
