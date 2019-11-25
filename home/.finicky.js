fa527297fc1c1dbc1e76374a77e38275b2182472
TravisCarden <travis.carden@gmail.com>
2019-11-25 13:35:00 -0500

    Added Brave Browser.

--- home/.finicky.js
+++ home/.finicky.js
@@ -14,6 +14,11 @@ module.exports = {
   // $ osascript -e 'id of app "ReplaceWithAppName"'
   // @see https://superuser.com/q/346369/1109136
   handlers: [
+    // Open links in Brave Browser when the option key is pressed.
+    {
+      match: ({ keys }) => keys.shift,
+      browser: "com.brave.Browser"
+    },
     // Open Mac app links in App Store app.
     {
       match: /apps.apple.com\/([^\/]*\/)?app\//,
